1ea79f3ed46755ae20aed908f27af505cf7c2b7a
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-01-27 02:34:23 +0530

    Time: 28 ms (39.81%) | Memory: 139.3 MB (25.39%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int>res;
+        deque<int>dq;
+        //deque me elements ki jagah unke element daalta hu taaki window shift karde samay deque ke left se nikalna hai ya nahi vo judge kar paaye
+
+        //setting up the first window in deque
+        for(int i=0; i<k; i++){
+
+            while(!dq.empty() && nums[i]>=nums[dq.back()]) dq.pop_back();
+
+            dq.push_back(i);
+        }
+
+        res.push_back(nums[dq.front()]);
+
+        for(int i=k; i<nums.size(); i++){
+            while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
+
+            while(!dq.empty() && nums[i]>=nums[dq.back()]) dq.pop_back();
+
+            dq.push_back(i);
+
+            res.push_back(nums[dq.front()]);
+        }
+
+        return res;
+    }
+};
