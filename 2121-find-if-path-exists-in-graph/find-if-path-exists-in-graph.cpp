fa1fc509b351d1c6475826d431149795376ef81d
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-01-29 02:18:02 +0530

    Time: 298 ms (52.94%) | Memory: 304.7 MB (47.16%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     bool dfs(int n, vector<vector<int>>&adj, int source , int destination, vector<bool>&visited){
+        
         if(source==destination) return true;
 
         visited[source] = true;
 
         for(int i=0; i<adj[source].size(); i++){
+            
             int neighbor = adj[source][i];
 
             if(!visited[neighbor]){
                 if(dfs(n, adj, neighbor, destination, visited)) return true;
             }
+        
         }
 
         return false;
     }
+
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        
         vector<vector<int>>adj(n);
 
         for(int i=0; i<edges.size(); i++){
