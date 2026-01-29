10121c3c8ebe6b3608d6438d00aa49c45ebcdcff
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-01-30 03:02:03 +0530

    Time: 8 ms (99.65%) | Memory: 96.4 MB (57.66%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j, vector<vector<bool>>&visited, bool& f){
+    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j, bool& f){
         if(i<0 || j<0 || i>=grid2.size() || j>=grid2[0].size()) return;
-        if(visited[i][j] || grid2[i][j]==0) return;
+        if(grid2[i][j]==0) return;
 
-        visited[i][j] = true;
         if(grid1[i][j]==0) f = false;
+        
+        grid2[i][j] = 0; // once visited mark 0, works like visited matrix
 
-        dfs(grid1, grid2, i+1, j, visited, f);
-        dfs(grid1, grid2, i-1, j, visited, f);
-        dfs(grid1, grid2, i, j+1, visited, f);
-        dfs(grid1, grid2, i, j-1, visited, f);
+        dfs(grid1, grid2, i+1, j, f);
+        dfs(grid1, grid2, i-1, j, f);
+        dfs(grid1, grid2, i, j+1, f);
+        dfs(grid1, grid2, i, j-1, f);
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        vector<vector<bool>>visited(grid2.size(), vector<bool>(grid2[0].size(), false));
 
         int count = 0;
         for(int i=0; i<grid2.size(); i++){
             for(int j=0; j<grid2[0].size(); j++){
-                if(grid2[i][j]==1 && !visited[i][j]){
+                if(grid2[i][j]==1){
                     bool f = true;
-                    dfs(grid1, grid2, i, j, visited, f);
+                    dfs(grid1, grid2, i, j, f);
                     if(f) count++;
                 }
             }
