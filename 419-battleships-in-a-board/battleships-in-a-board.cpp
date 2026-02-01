f8cdc84e3229c80088508752f9baba26d97be1c2
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-02-02 03:07:56 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (62.38%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/battleships-in-a-board.cpp
@@ -4,26 +4,11 @@ public:
         int n = board.size();
         int m = board[0].size();
 
-        //without using visited matrix
-        //visited me mark karne ki jagah seedha board me X ko . karte jaayenge
         int count = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(board[i][j]=='X'){
+                if(board[i][j]=='X' && (i==0 || board[i-1][j]=='.') && (j==0 || board[i][j-1]=='.')){
                     count++;
-                    board[i][j] = '.';
-
-                    int x = i, y = j+1;
-                    while(y<m && board[x][y]=='X'){
-                        board[x][y] = '.';
-                        y++;
-                    }
-
-                    x = i+1, y = j;
-                    while(x<n && board[x][y]=='X'){
-                        board[x][y] = '.';
-                        x++;
-                    }
                 }
             }
         }
