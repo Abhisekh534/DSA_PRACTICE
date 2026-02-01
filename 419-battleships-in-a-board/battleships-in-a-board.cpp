6247c94765b74a0929bcdc263feb3fe8b13d4a2f
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-02-02 02:50:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (31.01%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/battleships-in-a-board.cpp
@@ -3,6 +3,8 @@ public:
     int countBattleships(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
+
+        //using visited matrix
         vector<vector<bool>>visited(n, vector<bool>(m, false));
 
         int count = 0;
