4499f8d250b74865603d60e4c7a224c1d48e6592
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-02-02 03:08:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (83.01%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/battleships-in-a-board.cpp
@@ -7,9 +7,7 @@ public:
         int count = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(board[i][j]=='X' && (i==0 || board[i-1][j]=='.') && (j==0 || board[i][j-1]=='.')){
-                    count++;
-                }
+                if(board[i][j]=='X' && (i==0 || board[i-1][j]=='.') && (j==0 || board[i][j-1]=='.')) count++;
             }
         }
 
