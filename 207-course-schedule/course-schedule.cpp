07b82d3cd25ecd447f5762b8af717dcbedda6d33
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-02-03 04:22:52 +0530

    Time: 1 ms (84.24%) | Memory: 19.2 MB (68.39%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
+        vector<int>inDegree(numCourses);
 
         for(int i=0; i<prerequisites.size(); i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-        }
-
-        vector<int>inDegree(numCourses, 0);
-
-        for(int i=0; i<adj.size(); i++){
-            for(int j=0; j<adj[i].size(); j++){
-                inDegree[adj[i][j]]++;
-            }
+            inDegree[prerequisites[i][0]]++;
         }
 
         queue<int>q;
-        vector<int>topo;
-
+        int count = 0;
+        //instaed of using extra memory for topo sort , we can just count the number of nodes which will qualify for array topo
+        //and compare it with the number of nodes, count = number of nodes -> no cycle/deadlock
+        //count <> number of nodes -> cycle/deadlock present
+         
         for(int i=0; i<numCourses; i++){
             if(inDegree[i]==0){
                 q.push(i);
-                topo.push_back(i);
+                count++;
             }
         }
 
@@ -29,16 +26,19 @@ public:
             int node = q.front();
             q.pop();
             for(int i=0; i<adj[node].size(); i++){
+
                 int neighbor = adj[node][i];
                 inDegree[neighbor]--;
+
                 if(inDegree[neighbor]==0){
                     q.push(neighbor);
-                    topo.push_back(neighbor);
+                    count++;
                 }
+            
             }
         }
 
-        if(topo.size()==numCourses) return true;
+        if(count==numCourses) return true;
         return false;
     }
 };
