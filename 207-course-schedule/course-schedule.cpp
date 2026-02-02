491dd199d3d8e9d369bf96f4ed24d27acdda37c2
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-02-02 12:44:32 +0530

    Time: 3 ms (77.45%) | Memory: 19.4 MB (50.71%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+
+        for(int i=0; i<prerequisites.size(); i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+
+        vector<int>inDegree(numCourses, 0);
+
+        for(int i=0; i<adj.size(); i++){
+            for(int j=0; j<adj[i].size(); j++){
+                inDegree[adj[i][j]]++;
+            }
+        }
+
+        queue<int>q;
+        vector<int>topo;
+
+        for(int i=0; i<numCourses; i++){
+            if(inDegree[i]==0){
+                q.push(i);
+                topo.push_back(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(int i=0; i<adj[node].size(); i++){
+                int neighbor = adj[node][i];
+                inDegree[neighbor]--;
+                if(inDegree[neighbor]==0){
+                    q.push(neighbor);
+                    topo.push_back(neighbor);
+                }
+            }
+        }
+
+        if(topo.size()==numCourses) return true;
+        return false;
+    }
+};
