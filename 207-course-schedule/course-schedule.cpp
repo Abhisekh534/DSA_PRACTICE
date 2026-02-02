0dc1069de90bfc3f3964e9f2405e1baeee02f581
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-02-03 04:24:50 +0530

    Time: 11 ms (23.50%) | Memory: 19.2 MB (81.86%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
-        vector<int>inDegree(numCourses);
+        vector<int>inDegree(numCourses, 0);
 
         for(int i=0; i<prerequisites.size(); i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
