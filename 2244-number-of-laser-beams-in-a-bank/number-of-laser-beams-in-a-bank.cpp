979192f4c1e3cef37ead263b7817efa3c4b7731d
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-02-11 14:49:44 +0530

    Time: 12 ms (25.77%) | Memory: 27.3 MB (83.24%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -8,9 +8,9 @@ public:
             for(int j=0; j<bank[0].size(); j++){
                 if(bank[i][j]=='1') row++;
             }
-            
+
             if(row!=0){
-                if(previousRow!=0) ans+=(row*previousRow);
+                if(previousRow!=0) ans+=(row*previousRow);\
                 previousRow = row;
                 row = 0;
             }
