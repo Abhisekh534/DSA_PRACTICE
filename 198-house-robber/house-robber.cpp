97e8763fef6b8aa7082de0a768b57f752bf4f82b
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-02-12 13:52:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (21.57%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
+    int solve(vector<int>&nums, vector<int>&dp, int index){
+        if(index>=nums.size()) return 0;
+
+        if(dp[index]!=-1) return dp[index];
+
+        int take = nums[index] + solve(nums, dp, index+2);
+        int skip = solve(nums, dp, index+1);
+
+        return dp[index] = max(take, skip);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
+        vector<int>dp(n, -1);
 
-        if(n==1) return nums[0];
-
-        vector<int>tabu(n, 0);
-
-        tabu[0] = nums[0];
-        tabu[1] = max(nums[0], nums[1]);
-
-        for(int i=2; i<n; i++){
-            tabu[i] = max(tabu[i-2]+nums[i], tabu[i-1]);
-        }
-
-        return tabu[n-1];
+        return solve(nums, dp, 0);
     }
 };
