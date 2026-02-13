b8ea7e660285baffb40474dc804964836a73bdac
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-02-13 15:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (23.02%) - LeetSync

---
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        int dir = 0; //0 = north , 1 = west, 2 = south, 3 = east
+        int x = 0, y = 0;
+
+        for(int i=0; i<instructions.size(); i++){
+            if(instructions[i]=='L') dir = (dir+1)%4;
+            if(instructions[i]=='R') dir = ((dir-1)%4 + 4)%4;
+
+            if(instructions[i]=='G'){
+                if(dir==0) y++;
+                else if(dir==1) x--;
+                else if(dir==2) y--;
+                else x++;
+            }
+        }
+
+        if((x==0 && y==0) || dir!=0) return true;
+        return false;
+    }
+};
