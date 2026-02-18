0c2b45e31222941d821e743e6e31c0c5fa322516
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-02-19 02:13:01 +0530

    Time: 36 ms (66.67%) | Memory: 128.4 MB (19.70%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
@@ -1,13 +1,12 @@
 class Robot {
 public:
-    int dir = 0; //0 = east, 1 = north, 2 = west, 3 = south
     int stepp = 0;
-    int x = 0, y = 0, w, h, xx;
+    int w, h, x;
 
     Robot(int width, int height) {
         w = width;
         h = height;
-        xx = 2*((w-1) + (h-1));
+        x = 2*((w-1) + (h-1));
     }
     
     void step(int num) {
@@ -15,13 +14,13 @@ public:
     }
     
     vector<int> getPos() {
-        int z = stepp%xx;
+        int z = stepp%x;
         if(z<w){
             return {z, 0};
         }else if(z<(w+h-1)){
             return {w-1, z-w+1};
         }else if(z<(2*w+h-2)){
-            return {2*w-z+h-3, h-1};
+            return {2*w+h-z-3, h-1};
         }else{
             return {0, 2*h+2*w -z-4};
         }
@@ -29,12 +28,14 @@ public:
     
     string getDir() {
         if(stepp==0) return "East";
-        int z = stepp%xx;
 
-        if(z>0 && z<w) return "East";
-        if(z>0 && z<(w+h-1)) return "North";
-        else if(z>0 && z<(2*w+h-2)) return "West";
-        else return "South";
+        int z = stepp%x;
+
+        if(z==0) return "South";
+        if(z<w) return "East";
+        if(z<(w+h-1)) return "North";
+        if(z<(2*w+h-2)) return "West";
+        return "South";
     }
 };
 
