0863ab0a2c431f5e76b3d9f444892d6f3cbd5dac
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-03-15 02:14:42 +0530

    Time: 39 ms (9.80%) | Memory: 31.2 MB (17.52%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int solve(vector<vector<char>>&matrix, vector<vector<int>>&dp, int i, int j, int&maxi){
+    int solve(vector<vector<char>>&matrix, vector<vector<int>>&dp, int i, int j){
         if(i==0 || j==0){
-            return (matrix[i][j]=='1');
+            return dp[i][j] = (matrix[i][j]=='1');
         }
 
         if(dp[i][j]!=-1) return dp[i][j];
 
         if(matrix[i][j]=='0') return dp[i][j] = 0;
         else{
-            return dp[i][j] = 1 + min({solve(matrix, dp, i, j-1, maxi), solve(matrix, dp, i-1, j, maxi), solve(matrix, dp, i-1, j-1, maxi)});
+            return dp[i][j] = 1 + min({solve(matrix, dp, i, j-1), solve(matrix, dp, i-1, j), solve(matrix, dp, i-1, j-1)});
         }
 
     }
@@ -21,7 +21,7 @@ public:
         int maxi = 0;
         for(int i=m-1; i>-1; i--){
             for(int j=n-1; j>-1; j--){
-                maxi = max(maxi, solve(matrix, dp, i, j, maxi));
+                maxi = max(maxi, solve(matrix, dp, i, j));
             }
         }
 
