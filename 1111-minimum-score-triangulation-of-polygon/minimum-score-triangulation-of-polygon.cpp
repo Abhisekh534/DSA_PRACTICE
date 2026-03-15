6cdcc765cbbad7fb3c95fafa1ed3449854d7dbb7
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-03-16 03:00:32 +0530

    Time: 2 ms (63.51%) | Memory: 11.3 MB (42.94%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -8,6 +8,9 @@ public:
         int mini = INT_MAX;
 
         for(int i=n-1; i>-1; i--){
+            //i ko ulta chalana hai
+            //why?? eg, i=0 j=6 k=2, then we need dp[0][2] and dp[2][6], but we don't have real value of dp[2][6] not placeholder 0
+            //knowing real value of dp[2][6] in this pass(i=0) is only possible if we have done the pass of i=2 before i=0 
             for(int j=i+2; j<n; j++){
                 int mini = INT_MAX;
                 for(int k=i+1; k<j; k++){
