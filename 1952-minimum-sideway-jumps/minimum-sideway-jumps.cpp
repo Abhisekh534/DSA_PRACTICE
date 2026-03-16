dabd604e09dc09da0dc8a0761f9d27014759aeab
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-03-17 00:30:47 +0530

    Time: 46 ms (79.88%) | Memory: 191.8 MB (89.94%) - LeetSync

--- 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int solve(vector<int>&obstacles, vector<vector<int>>&dp, int i, int position){
-        int n = obstacles.size();
-        if(i==n-1) return 0;
-
-        if(dp[i][position-1]!=-1) return dp[i][position-1];
-
-        int mini = INT_MAX;
-        if(obstacles[i+1]==position){
-            for(int j=1; j<4; j++){
-                if(j!=position && j!=obstacles[i]){
-                    mini = min(mini, 1 + solve(obstacles, dp, i+1, j));
-                }
-            }
-        }else mini = solve(obstacles, dp, i+1, position);
-
-        return dp[i][position-1] = mini;
-    }
     int minSideJumps(vector<int>& obstacles) {
         int n = obstacles.size();
-        vector<vector<int>>dp(n+1, vector<int>(3, -1));
-        return solve(obstacles, dp, 0, 2);
+        vector<int>dp = {1, 0, 1};
+        
+        for(int i=1; i<n; i++){
+            if(obstacles[i]!=0) dp[obstacles[i]-1] = 1e9;
+
+            if(obstacles[i]!=1) dp[0] = min({dp[0], 1+dp[1], 1+dp[2]});
+            if(obstacles[i]!=2) dp[1] = min({dp[1], 1+dp[0], 1+dp[2]});
+            if(obstacles[i]!=3) dp[2] = min({dp[2], 1+dp[0], 1+dp[1]});
+        }
+
+        return min({dp[0], dp[1], dp[2]});
     }
 };
