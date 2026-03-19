23d9c2fb013b3f4af7a572c5e65dddad9ac7e081
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-03-20 02:42:15 +0530

    Time: 175 ms (33.82%) | Memory: 14.7 MB (35.97%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
+    int solve(vector<int>&nums, vector<int>&LIS, int i){
+        int n = nums.size();
+
+        if(LIS[i]!=1) return LIS[i];
+
+        for(int j=i+1; j<n; j++){
+            if(nums[i]<nums[j]) LIS[i] = max(LIS[i], 1+solve(nums, LIS, j));
+        }
+
+        return LIS[i];
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<int>LIS(n, 1);
 
         int maxi = 1;
-        for(int i=n-2; i>-1; i--){
-            for(int j=i+1; j<n; j++){
-                if(nums[i]<nums[j]) LIS[i] = max(LIS[i], 1 + LIS[j]);
-            }
-            maxi = max(maxi, LIS[i]);
+
+        for(int i=0; i<n-1; i++){
+            maxi = max(maxi, solve(nums, LIS, i));
         }
 
         return maxi;
