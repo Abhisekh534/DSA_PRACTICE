7a2fd08426babcaf7a26113da7e19fa56cfec4b7
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-03-29 23:08:43 +0530

    Time: 3 ms (89.80%) | Memory: 130.2 MB (27.20%) - LeetSync

---
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<long long>rows(m);
+        for(int i=0; i<m; i++){
+            long long sum = 0;
+            for(int j=0; j<n; j++) sum+=(grid[i][j]);
+            rows[i] = sum;
+        }
+
+        vector<long long>columns(n);
+
+        for(int j=0; j<n; j++){
+            long long sum = 0;
+            for(int i=0; i<m; i++) sum+=(grid[i][j]);
+            columns[j] = sum;
+        }
+
+        long long pre = 0;
+        long long post = 0;
+
+        for(int i=0; i<m; i++){
+            pre+=(rows[i]);
+        }
+
+
+        for(int i=m-1; i>0; i--){
+            pre-=(rows[i]);
+            post+=(rows[i]);
+
+            if(pre==post) return true;
+        }
+
+        pre = 0;
+        post = 0;
+
+        for(int i=0; i<n; i++){
+            pre+=(columns[i]);
+        }
+
+
+        for(int i=n-1; i>0; i--){
+            pre-=(columns[i]);
+            post+=(columns[i]);
+
+            if(pre==post) return true;
+        }
+
+        return false;
+    }
+};
