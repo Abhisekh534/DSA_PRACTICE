38c886f2ba05770aab67ffa4547613a9549eddba
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-04-01 15:03:41 +0530

    Time: 14 ms (9.37%) | Memory: 127.9 MB (22.40%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.cpp
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -11,16 +11,14 @@ public:
 
         if((total-mSum)>6*n || (total-mSum)<n) return ans;
 
-        int rem = (total-mSum);
-        int x = rem/n;
+        int x = (total-mSum)/n;
 
-        int b = rem%n;
+        int b = (total-mSum)%n;
         int a = n-b;
 
         for(int i=0; i<a; i++) ans.push_back(x);
         for(int i=0; i<b; i++) ans.push_back(x+1);
 
         return ans;
-
     }
 };
