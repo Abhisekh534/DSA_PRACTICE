757d787cc4175aa021d189689e368c21947a8218
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-05-11 02:22:51 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (89.44%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int ocolor, vector<vector<bool>>&visited){
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int ocolor){
         int n = image.size();
         int m = image[0].size();
-        if(sr<0 || sr==n || sc<0 || sc==m || visited[sr][sc] || image[sr][sc]!=ocolor) return;
+        if(sr<0 || sr==n || sc<0 || sc==m || image[sr][sc]!=ocolor) return;
 
-        visited[sr][sc] = true;
         image[sr][sc] = color;
 
-        dfs(image, sr, sc+1, color, ocolor, visited);
-        dfs(image, sr, sc-1, color, ocolor, visited);
-        dfs(image, sr+1, sc, color, ocolor, visited);
-        dfs(image, sr-1, sc, color, ocolor, visited);
+        dfs(image, sr, sc+1, color, ocolor);
+        dfs(image, sr, sc-1, color, ocolor);
+        dfs(image, sr+1, sc, color, ocolor);
+        dfs(image, sr-1, sc, color, ocolor);
 
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
         int ocolor = image[sr][sc];
+
+        // without this small optimization , we would have needed a visited matrix
         if(ocolor==color) return image;
 
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-        dfs(image, sr, sc, color, ocolor, visited);
+        dfs(image, sr, sc, color, ocolor);
 
 
         return image;
