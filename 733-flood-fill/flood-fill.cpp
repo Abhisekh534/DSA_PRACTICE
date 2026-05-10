c85c5e2e310ca43137025b6aab1dd37222024fb9
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-05-11 02:09:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (42.99%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -6,13 +6,13 @@ public:
         if(sr<0 || sr==n || sc<0 || sc==m || visited[sr][sc] || image[sr][sc]!=ocolor) return;
 
         visited[sr][sc] = true;
+        image[sr][sc] = color;
 
         dfs(image, sr, sc+1, color, ocolor, visited);
         dfs(image, sr, sc-1, color, ocolor, visited);
         dfs(image, sr+1, sc, color, ocolor, visited);
         dfs(image, sr-1, sc, color, ocolor, visited);
 
-        image[sr][sc] = color;
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
