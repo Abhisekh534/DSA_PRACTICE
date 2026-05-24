c8f9ac60547a58d4f64ca55cfc73d338c50c5ce2
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-05-24 17:39:33 +0530

    Time: 0 ms (100.00%) | Memory: 102.1 MB (41.72%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&grid, int i, int j, vector<vector<int>>&visited){
-        int n = grid.size();
-        int m = grid[0].size();
-
-        if(i<0 || i==n || j<0 || j==m || grid[i][j]==0) return 1;
-
-        if(visited[i][j]) return 0;
-
-        visited[i][j] = true;
-        
-        int left = dfs(grid, i, j-1, visited);
-        int right = dfs(grid, i, j+1, visited);
-        int top = dfs(grid, i-1, j, visited);
-        int bottom = dfs(grid, i+1, j, visited);
-
-        return left + right + top + bottom;
-    }
     int islandPerimeter(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<int>>visited(n, vector<int>(m, false));
+        int perimeter = 0;
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j]==1) return dfs(grid, i, j, visited);
+                if(grid[i][j]==1){
+                    perimeter+=4;
+
+                    if(i>0 && grid[i-1][j]==1) perimeter-=2;
+
+                    if(j>0 && grid[i][j-1]==1) perimeter-=2;
+                }
             }
         }
-        return 0; 
+
+        return perimeter;
     }
 };
