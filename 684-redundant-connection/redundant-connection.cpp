1652fb248ca40ee150915b47d2320e4bc2b9be53
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-06-18 01:02:45 +0530

    Time: 19 ms (8.72%) | Memory: 43.3 MB (5.60%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool dfs(int source, int dest, vector<vector<int>>&adj, vector<int>&vis){
+
+        if(source==dest) return true;
+
+        vis[source] = true;
+
+        for(int x : adj[source]){
+            if(!vis[x]){
+                if(dfs(x, dest, adj, vis)) return true;
+            }
+        }
+
+        return false;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+
+        vector<vector<int>>adj(n+1);
+
+        for(int i=0; i<n; i++){
+            int source = edges[i][0];
+            int dest = edges[i][1];
+
+            //check previous indirect connection(using dfs) before adding this direct connection 
+            
+            vector<int>vis(n+1, false);
+            
+            if(!adj[source].empty() && !adj[dest].empty()){
+                if(dfs(source, dest, adj, vis)) return {source, dest};
+            }
+
+            //because the control did not go to the return statement, therefore no previous connection
+            //now make this connection
+
+            adj[source].push_back(dest);
+            adj[dest].push_back(source);
+        }
+
+        return {};
+    }
+};
