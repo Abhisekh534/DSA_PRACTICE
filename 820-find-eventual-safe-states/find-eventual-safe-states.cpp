e357fedf085d551d2aa729e17316dfc07c5626ea
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-06-18 02:51:27 +0530

    Time: 61 ms (12.67%) | Memory: 66.3 MB (19.79%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -4,7 +4,7 @@ public:
         int n = graph.size();
         vector<vector<int>>graphM(n);
 
-        vector<int>outDegree(n, 0);
+        vector<int>outDegree(n);
 
         for(int i=0; i<n; i++){
             for(int j=0; j<graph[i].size(); j++){
@@ -14,42 +14,32 @@ public:
         }
 
         queue<int>q;
-        vector<int>isSafe(n, false);
-        //isSafe na use karke direct bhi safe nodes ko vector<ans> mein daal sakte hain
-        //par vo sorted nahi hoga aur sort karne mein time complexity badh jaayegi
-        //isSafe se extra memory lagegi par vector<ans> mein ek pass mein saare nodes sorted order me aa jaayenge
+        vector<int>ans;
 
         for(int i=0; i<n; i++){
             if(outDegree[i]==0){
                 q.push(i);
-                isSafe[i] = true;
+                ans.push_back(i);
             }
         }
-
 
         while(!q.empty()){
             int node = q.front();
             q.pop();
 
             for(int i=0; i<graphM[node].size(); i++){
+                int previous = graphM[node][i];
 
-                int predecessor = graphM[node][i];
+                outDegree[previous]--;
 
-                outDegree[predecessor]--;
-
-                if(outDegree[predecessor]==0){
-                    q.push(predecessor);
-                    isSafe[predecessor] = true;
+                if(outDegree[previous]==0){
+                    q.push(previous);
+                    ans.push_back(previous);
                 }
-
             }
         }
 
-        vector<int>ans;
-
-        for(int i=0; i<n; i++){
-            if(isSafe[i]) ans.push_back(i);
-        }
+        sort(ans.begin(), ans.end());
 
         return ans;
     }
