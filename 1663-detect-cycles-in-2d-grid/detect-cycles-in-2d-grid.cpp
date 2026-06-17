53ef8349e0471ad060a003d025bb368260ac156c
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-06-18 02:07:19 +0530

    Time: 16 ms (82.61%) | Memory: 57.3 MB (79.38%) - LeetSync

---
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool dfs(int i, int j, int parx, int pary, vector<vector<char>>&grid, vector<vector<bool>>&visited){
+        int n = grid.size();
+        int m = grid[0].size();
+
+        if(i<0 || i==n || j<0 || j==m || grid[i][j]!=grid[parx][pary]) return false;
+
+        if(visited[i][j]) return true;
+
+        visited[i][j] = true;
+
+        int result = false;
+
+        int kx[4] = {0, 0, 1, -1};
+        int ky[4] = {-1, 1, 0, 0};
+
+        for(int k=0; k<4; k++){
+            int nx = i + kx[k];
+            int ny = j + ky[k];
+
+            if(!(nx==parx && ny==pary)) result = result || dfs(nx, ny, i, j, grid, visited);
+        }
+
+        return result;
+    }
+    bool containsCycle(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<bool>>visited(n, vector<bool>(m, false));
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!visited[i][j] && dfs(i, j, i,j, grid, visited)) return true;
+            }
+        }
+
+        return false;
+    }
+};
