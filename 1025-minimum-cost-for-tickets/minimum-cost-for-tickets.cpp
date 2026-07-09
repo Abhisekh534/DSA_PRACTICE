1db031786c805b6f936fafac95dd3be47708755d
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-07-09 14:25:24 +0530

    Time: 1 ms (36.45%) | Memory: 14.2 MB (27.65%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int solve(vector<int>&days, vector<int>&costs, vector<int>&dp, int i){
-        //index function parameter hai, amount function parameter nahi hai
-        //amount ko function parameter banane se dp vector confuse ho jaayega
+    int solve(vector<int>&days, vector<int>&costs, int i, vector<int>&dp){
         int n = days.size();
+
         if(i>=n) return 0;
 
         if(dp[i]!=-1) return dp[i];
-        
-        int one = costs[0] + solve(days, costs, dp, i+1);
-        
+
+        int one = costs[0] + solve(days, costs, i+1, dp);
+
         int x = i;
-        while(x<n && days[x]<=days[i]+6) x++;
-        int seven = costs[1] + solve(days, costs, dp, x);
+        while(x<n && days[x]<days[i]+7) x++;
+        int seven = costs[1] + solve(days, costs, x, dp);
 
         x = i;
-        while(x<n && days[x]<=days[i]+29) x++;
-        int thirty = costs[2] + solve(days, costs, dp, x);
+        while(x<n && days[x]<days[i]+30) x++;
+        int thirty = costs[2] + solve(days, costs, x, dp);
 
         return dp[i] = min({one, seven, thirty});
+
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
         vector<int>dp(n, -1);
 
-        return solve(days, costs, dp, 0);
+        return solve(days, costs, 0, dp);
     }
 };
