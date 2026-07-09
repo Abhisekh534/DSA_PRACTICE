9a98944bae95eb082e31675a75ba7c289f17293d
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-07-09 11:37:03 +0530

    Time: 30 ms (92.93%) | Memory: 13.2 MB (69.62%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     int numSquares(int n) {
         //bottom up
-        vector<int>squares;
-
-        for(int i=1; i*i<=n; i++){
-            squares.push_back(i*i);
-        }
-
+        
         vector<int>dp(n+1, INT_MAX);
         dp[0] = 0; //prevents overflow when i is perfect square and 'square' = i
         
         for(int i=1; i<=n; i++){
 
             int mini = INT_MAX;
-            for(int square : squares){
-                if(square>i) break;
-                mini = min(mini, 1+dp[i-square]);
+            for(int j=1; j*j<=i; j++){
+                mini = min(mini, 1+dp[i-j*j]);
             }
             dp[i] = mini;
 
