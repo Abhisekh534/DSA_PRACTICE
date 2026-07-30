54dac30cac2eaefb2fa4a83662283eb12429368f
Abhisekh534 <146093786+Abhisekh534@users.noreply.github.com>
2026-07-30 23:34:08 +0530

    Time: 14 ms (25.62%) | Memory: 90.9 MB (10.07%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,12 +1,17 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        int x = 1;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]>x) return x;
-            if(nums[i]==x) x++;
+        int n = nums.length;
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<n; i++){
+            if(nums[i]>0) set.add(nums[i]);
         }
 
-        return x;
+        for(int i=1; i<=n; i++){
+            if(!set.contains(i)) return i;
+        }
+
+        return n+1;
     }
 }
